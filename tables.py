0e5b082387669634fd6c55f05232aba5a011224a
zomorodiyan <mehrdadzomorodiyan@gmail.com>
2022-06-08 16:39:29 -0500

    add comment

--- tables.py
+++ tables.py
@@ -3,7 +3,6 @@ label = ['c1','c2','c3','c4']
 in_ =  [['a','x',1,2],\
         ['b','x',3,4],\
         ['c','x',5,6]]
-
 desired_out = [['x','a','c3',1],\
         ['x','a','c4',2],\
         ['x','b','c3',3],\
@@ -17,7 +16,7 @@ for row in in_:
   for i in range(2,4): # {2,3}: indexes of {c3,c4}
     out.append([row[1],row[0],label[i],row[i]])
 
-# generate output
+# assert output is right
 for i in range(len(out)):
   for j in range(len(out[0])):
     assert(out[i][j]==desired_out[i][j])
