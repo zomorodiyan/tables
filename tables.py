86b68b22507a2bfb0d7a123913b91eadff1d8965
zomorodiyan <mehrdadzomorodiyan@gmail.com>
2022-06-08 16:37:53 -0500

    pass

---
+++ tables.py
@@ -0,0 +1,24 @@
+# define variables
+label = ['c1','c2','c3','c4']
+in_ =  [['a','x',1,2],\
+        ['b','x',3,4],\
+        ['c','x',5,6]]
+
+desired_out = [['x','a','c3',1],\
+        ['x','a','c4',2],\
+        ['x','b','c3',3],\
+        ['x','b','c4',4],\
+        ['x','c','c3',5],\
+        ['x','c','c4',6]]
+
+# generate output
+out = []
+for row in in_:
+  for i in range(2,4): # {2,3}: indexes of {c3,c4}
+    out.append([row[1],row[0],label[i],row[i]])
+
+# generate output
+for i in range(len(out)):
+  for j in range(len(out[0])):
+    assert(out[i][j]==desired_out[i][j])
+
